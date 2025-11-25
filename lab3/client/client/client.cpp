24388b1359e3ad2a7c70ff083ffd70b48d9600c9
KseniaRastor <133050124+KseniaRastor@users.noreply.github.com>
2025-11-25 22:42:15 +0300

    lab3

---
+++ lab3/client/client/client.cpp
@@ -0,0 +1,433 @@
+﻿//#define _WINSOCK_DEPRECATED_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <winsock2.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//#pragma comment(lib, "ws2_32.lib")
+//
+//#define MAXLINE 512
+//#define DEFAULT_PORT 6000
+//#define DEFAULT_HOST "127.0.0.1"
+//
+////--------------------------------------------------
+//void clear_input_buffer() {
+//    int c;
+//    while ((c = getchar()) != '\n' && c != EOF);
+//}
+//
+////--------------------------------------------------
+//int main()
+//{
+//    char server_host[64] = DEFAULT_HOST;
+//    int server_port = DEFAULT_PORT;
+//    char input[64];
+//
+//    // Интерактивная настройка подключения
+//    printf("MySQL Client Configuration\n");
+//    printf("==========================\n");
+//
+//    // Ввод хоста
+//    printf("Server host: ", DEFAULT_HOST);
+//    if (fgets(input, sizeof(input), stdin) != NULL) {
+//        // Убираем символ новой строки
+//        input[strcspn(input, "\n")] = 0;
+//
+//        // Если пользователь ввел что-то (не просто Enter)
+//        if (strlen(input) > 0) {
+//            strncpy_s(server_host, sizeof(server_host), input, _TRUNCATE);
+//        }
+//    }
+//
+//    // Ввод порта
+//    printf("Server port: ", DEFAULT_PORT);
+//    if (fgets(input, sizeof(input), stdin) != NULL) {
+//        input[strcspn(input, "\n")] = 0;
+//        if (strlen(input) > 0) {
+//            server_port = atoi(input);
+//            if (server_port <= 0 || server_port > 65535) {
+//                printf("Invalid port! Using default %d\n", DEFAULT_PORT);
+//                server_port = DEFAULT_PORT;
+//            }
+//        }
+//    }
+//
+//    printf("\nConnecting to %s:%d...\n\n", server_host, server_port);
+//
+//    WSADATA wsaData;
+//    SOCKET sockfd;
+//    struct sockaddr_in serv_addr;
+//    char buffer[MAXLINE];
+//    int bytesReceived;
+//
+//    // Initialize Winsock
+//    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+//        printf("WSAStartup failed.\n");
+//        return 1;
+//    }
+//
+//    // Create socket
+//    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+//        printf("Cannot create socket\n");
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    // Setup server address
+//    serv_addr.sin_family = AF_INET;
+//    serv_addr.sin_addr.s_addr = inet_addr(server_host);
+//    serv_addr.sin_port = htons(server_port);
+//
+//    // Connect to server
+//    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR) {
+//        printf("Cannot connect to server at %s:%d\n", server_host, server_port);
+//        printf("Error code: %d\n", WSAGetLastError());
+//        closesocket(sockfd);
+//        WSACleanup();
+//        return 1;
+//    }
+//
+//    printf("Connected to server at %s:%d\n", server_host, server_port);
+//    printf("Enter SQL queries (type 'quit' or 'exit' to quit):\n\n");
+//
+//    // Main interaction loop
+//    while (1) {
+//        printf("SQL> ");
+//
+//        // Read input
+//        if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+//            break;
+//        }
+//
+//        // Remove newline
+//        buffer[strcspn(buffer, "\n")] = 0;
+//
+//        // Check for exit commands
+//        if (strcmp(buffer, "quit") == 0 || strcmp(buffer, "exit") == 0) {
+//            break;
+//        }
+//
+//        // Skip empty queries
+//        if (strlen(buffer) == 0) {
+//            continue;
+//        }
+//
+//        // Send query to server
+//        if (send(sockfd, buffer, strlen(buffer) + 1, 0) == SOCKET_ERROR) {
+//            printf("Send error\n");
+//            break;
+//        }
+//
+//        // Receive response from server
+//        bytesReceived = recv(sockfd, buffer, MAXLINE - 1, 0);
+//        if (bytesReceived > 0) {
+//            buffer[bytesReceived] = '\0';
+//            printf("Server response:\n%s\n\n", buffer);
+//        }
+//        else if (bytesReceived == 0) {
+//            printf("Server closed connection\n");
+//            break;
+//        }
+//        else {
+//            printf("Receive error\n");
+//            break;
+//        }
+//    }
+//
+//    closesocket(sockfd);
+//    WSACleanup();
+//    printf("Client terminated.\n");
+//    return 0;
+//}
+
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <winsock2.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define MAXLINE 512
+#define DEFAULT_PORT 6000
+#define DEFAULT_HOST "127.0.0.1"
+
+//--------------------------------------------------
+void clear_input_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+//--------------------------------------------------
+void print_menu() {
+    printf("\n----- MySQL Database Client Menu -----\n");
+    printf("1. Show all tables\n");
+    printf("2. Show table structure\n");
+    printf("3. Show table data\n");
+    printf("4. Add new record\n");
+    printf("5. Delete record\n");
+    printf("6. Show database status\n");
+    printf("7. Exit\n");
+    printf("Choose an option (1-7): ");
+}
+
+//--------------------------------------------------
+int send_query_and_receive(SOCKET sockfd, const char* query, char* buffer) {
+    // Send query to server
+    if (send(sockfd, query, strlen(query) + 1, 0) == SOCKET_ERROR) {
+        printf("Send error\n");
+        return -1;
+    }
+
+    // Receive response from server
+    int bytesReceived = recv(sockfd, buffer, MAXLINE - 1, 0);
+    if (bytesReceived > 0) {
+        buffer[bytesReceived] = '\0';
+        return 0;
+    }
+    else if (bytesReceived == 0) {
+        printf("Server closed connection\n");
+        return -1;
+    }
+    else {
+        printf("Receive error\n");
+        return -1;
+    }
+}
+
+//--------------------------------------------------
+void show_all_tables(SOCKET sockfd) {
+    char buffer[MAXLINE];
+    const char* query = "SHOW TABLES";
+
+    printf("\n--- Showing all tables ---\n");
+
+    if (send_query_and_receive(sockfd, query, buffer) == 0) {
+        printf("%s\n", buffer);
+    }
+}
+
+//--------------------------------------------------
+void show_table_structure(SOCKET sockfd) {
+    char table_name[64];
+    char query[128];
+    char buffer[MAXLINE];
+
+    printf("\nEnter table name: ");
+    scanf("%63s", table_name);
+    clear_input_buffer();
+
+    sprintf_s(query, sizeof(query), "DESCRIBE %s", table_name);
+
+    printf("\n--- Structure of table '%s' ---\n", table_name);
+
+    if (send_query_and_receive(sockfd, query, buffer) == 0) {
+        printf("%s\n", buffer);
+    }
+}
+
+//--------------------------------------------------
+void show_table_data(SOCKET sockfd) {
+    char table_name[64];
+    char query[128];
+    char buffer[MAXLINE];
+
+    printf("\nEnter table name: ");
+    scanf("%63s", table_name);
+    clear_input_buffer();
+
+    sprintf_s(query, sizeof(query), "SELECT * FROM %s", table_name);
+
+    printf("\n--- Data from table '%s' ---\n", table_name);
+
+    if (send_query_and_receive(sockfd, query, buffer) == 0) {
+        printf("%s\n", buffer);
+    }
+}
+
+//--------------------------------------------------
+void add_new_user(SOCKET sockfd) {
+    char name[64];
+    int age;
+    char query[256];
+    char buffer[MAXLINE];
+
+    printf("\n--- Add new user ---\n");
+    printf("Enter user name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strcspn(name, "\n")] = 0;
+
+    printf("Enter user age: ");
+    scanf("%d", &age);
+    clear_input_buffer();
+
+    sprintf_s(query, sizeof(query),
+        "INSERT INTO users (name, age) VALUES ('%s', %d)",
+        name, age);
+
+    if (send_query_and_receive(sockfd, query, buffer) == 0) {
+        printf("Result: %s\n", buffer);
+    }
+}
+
+//--------------------------------------------------
+void delete_user(SOCKET sockfd) {
+    int user_id;
+    char query[128];
+    char buffer[MAXLINE];
+
+    printf("\n--- Delete user ---\n");
+    printf("Enter user ID to delete: ");
+    scanf("%d", &user_id);
+    clear_input_buffer();
+
+    sprintf_s(query, sizeof(query), "DELETE FROM users WHERE id = %d", user_id);
+
+    if (send_query_and_receive(sockfd, query, buffer) == 0) {
+        printf("Result: %s\n", buffer);
+    }
+}
+
+//--------------------------------------------------
+void show_database_status(SOCKET sockfd) {
+    char buffer[MAXLINE];
+
+    printf("\n--- Database Status ---\n");
+
+    // Show database version
+    if (send_query_and_receive(sockfd, "SELECT VERSION() as DatabaseVersion", buffer) == 0) {
+        printf("Database Version:\n%s\n", buffer);
+    }
+
+    // Show current database
+    if (send_query_and_receive(sockfd, "SELECT DATABASE() as CurrentDatabase", buffer) == 0) {
+        printf("Current Database:\n%s\n", buffer);
+    }
+}
+
+//--------------------------------------------------
+int main() {
+    char server_host[64] = DEFAULT_HOST;
+    int server_port = DEFAULT_PORT;
+    char input[64];
+
+    // Интерактивная настройка подключения
+    printf("MySQL Client Configuration\n");
+    printf("-------------------------\n");
+
+    // Ввод хоста
+    printf("Server host: ", DEFAULT_HOST);
+    if (fgets(input, sizeof(input), stdin) != NULL) {
+        input[strcspn(input, "\n")] = 0;
+        if (strlen(input) > 0) {
+            strncpy_s(server_host, sizeof(server_host), input, _TRUNCATE);
+        }
+    }
+
+    // Ввод порта
+    printf("Server port: ", DEFAULT_PORT);
+    if (fgets(input, sizeof(input), stdin) != NULL) {
+        input[strcspn(input, "\n")] = 0;
+        if (strlen(input) > 0) {
+            server_port = atoi(input);
+            if (server_port <= 0 || server_port > 65535) {
+                printf("Invalid port! Using default %d\n", DEFAULT_PORT);
+                server_port = DEFAULT_PORT;
+            }
+        }
+    }
+
+    printf("\nConnecting to %s:%d...\n", server_host, server_port);
+
+    WSADATA wsaData;
+    SOCKET sockfd;
+    struct sockaddr_in serv_addr;
+
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup failed.\n");
+        return 1;
+    }
+
+    // Create socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+        printf("Cannot create socket\n");
+        WSACleanup();
+        return 1;
+    }
+
+    // Setup server address
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(server_host);
+    serv_addr.sin_port = htons(server_port);
+
+    // Connect to server
+    if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR) {
+        printf("Cannot connect to server at %s:%d\n", server_host, server_port);
+        printf("Error code: %d\n", WSAGetLastError());
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Connected to server at %s:%d\n", server_host, server_port);
+
+    // Create users table if not exists
+    char init_query[] = "CREATE TABLE IF NOT EXISTS users ("
+        "id INT AUTO_INCREMENT PRIMARY KEY, "
+        "name VARCHAR(100) NOT NULL, "
+        "email VARCHAR(100) UNIQUE NOT NULL, "
+        "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)";
+    char buffer[MAXLINE];
+    send_query_and_receive(sockfd, init_query, buffer);
+
+    // Main menu loop
+    int choice;
+    while (1) {
+        print_menu();
+
+        if (scanf("%d", &choice) != 1) {
+            clear_input_buffer();
+            printf("Invalid input! Please enter a number.\n");
+            continue;
+        }
+        clear_input_buffer();
+
+        switch (choice) {
+        case 1:
+            show_all_tables(sockfd);
+            break;
+        case 2:
+            show_table_structure(sockfd);
+            break;
+        case 3:
+            show_table_data(sockfd);
+            break;
+        case 4:
+            add_new_user(sockfd);
+            break;
+        case 5:
+            delete_user(sockfd);
+            break;
+        case 6:
+            show_database_status(sockfd);
+            break;
+        case 7:
+            closesocket(sockfd);
+            WSACleanup();
+            return 0;
+        default:
+            printf("Invalid option! Please choose 1-7.\n");
+            break;
+        }
+    }
+
+    closesocket(sockfd);
+    WSACleanup();
+    return 0;
+}
---
+++ lab3/server/server/server.cpp
@@ -0,0 +1,359 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <string>
+#include <algorithm>
+
+// MySQL C API - более стабильный
+#include <mysql.h>
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "libmysql.lib")
+
+#define MAXLINE 4096
+#define DEFAULT_PORT 6000
+#define BUFFER_SIZE 4096
+
+// Configuration
+int SERV_TCP_PORT = DEFAULT_PORT;
+
+// MySQL configuration
+const char* DB_HOST = "127.0.0.1";
+const char* DB_USER = "root";
+const char* DB_PASS = "Root123!";
+const char* DB_NAME = "test_db";
+const int DB_PORT = 3306;
+
+SOCKET sockfd;
+
+// Function declarations
+int readline(SOCKET fd, char* ptr, int maxlen);
+int writen(SOCKET fd, const char* ptr, int nbytes);
+void WriteToLog(const char* message);
+MYSQL* create_mysql_session();
+const char* execute_sql_query(MYSQL* mysql, const char* query);
+DWORD WINAPI ClientHandler(LPVOID lpParam);
+
+//--------------------------------------------------
+void WriteToLog(const char* message) {
+    printf("[LOG] %s\n", message);
+
+    FILE* logfile = fopen("serv.log", "a");
+    if (logfile) {
+        time_t now = time(NULL);
+        char time_str[64];
+        ctime_s(time_str, sizeof(time_str), &now);
+        time_str[strlen(time_str) - 1] = '\0';
+        fprintf(logfile, "[%s] %s\n", time_str, message);
+        fclose(logfile);
+    }
+}
+
+//--------------------------------------------------
+int readline(SOCKET fd, char* ptr, int maxlen) {
+    static char buffer[BUFFER_SIZE];
+    static int buf_pos = 0;
+    static int buf_len = 0;
+    int n = 0;
+
+    while (n < maxlen - 1) {
+        // Если буфер пуст - читаем новый блок
+        if (buf_pos >= buf_len) {
+            buf_len = recv(fd, buffer, BUFFER_SIZE, 0);
+            if (buf_len <= 0) return buf_len;  // Ошибка
+            buf_pos = 0;
+        }
+
+        // Копируем из буфера, пока не \0
+        while (buf_pos < buf_len && n < maxlen - 1) {
+            char c = buffer[buf_pos++];
+            ptr[n++] = c;
+            if (c == '\0') return n;  // Конец строки
+        }
+    }
+
+    ptr[n] = '\0';
+    return n;
+}
+
+//--------------------------------------------------
+int writen(SOCKET fd, const char* ptr, int nbytes) {
+    if (!ptr || nbytes <= 0) return -1;
+
+    int nleft = nbytes;
+    int nwritten;
+
+    while (nleft > 0) {
+        nwritten = send(fd, ptr, nleft, 0);
+        if (nwritten <= 0) {
+            return nwritten;
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+    return nbytes - nleft;      // 0 при успешной записи всех байт
+}
+
+//--------------------------------------------------
+MYSQL* create_mysql_session() {
+    WriteToLog("Creating MySQL session with C API...");
+
+    MYSQL* mysql = mysql_init(NULL);
+    if (!mysql) {
+        WriteToLog("MySQL initialization failed");
+        return nullptr;
+    }
+
+    // Настраиваем кодировку
+    mysql_options(mysql, MYSQL_SET_CHARSET_NAME, "utf8mb4");
+
+    WriteToLog("Attempting MySQL connection...");
+
+    MYSQL* conn = mysql_real_connect(mysql, DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT, NULL, 0);
+
+    if (!conn) {
+        char error_msg[512];
+        sprintf_s(error_msg, sizeof(error_msg), "MySQL Connection Error: %s", mysql_error(mysql));
+        WriteToLog(error_msg);
+        mysql_close(mysql);
+        return nullptr;
+    }
+
+    WriteToLog("MySQL connection established successfully");
+    return conn;
+}
+
+//--------------------------------------------------
+const char* execute_sql_query(MYSQL* mysql, const char* query) {
+    if (!query || strlen(query) == 0) {
+        return "Error: Empty query";
+    }
+
+    if (!mysql) {
+        return "Error: No database connection";
+    }
+
+    static char response[MAXLINE];
+    response[0] = '\0';
+
+    // Выполняем запрос
+    if (mysql_query(mysql, query)) {
+        sprintf_s(response, sizeof(response), "MySQL Error: %s", mysql_error(mysql));
+        return response;
+    }
+
+    // Получаем результат
+    MYSQL_RES* result = mysql_store_result(mysql);
+
+    if (result) {
+        // SELECT запрос - есть результат
+        std::string result_str;
+        MYSQL_ROW row;
+        MYSQL_FIELD* fields;
+        int num_fields = mysql_num_fields(result);
+
+        // Заголовки столбцов
+        fields = mysql_fetch_fields(result);
+        for (int i = 0; i < num_fields; i++) {
+            if (i > 0) result_str += " | ";
+            result_str += fields[i].name;
+        }
+        result_str += "\n";
+
+        // Разделитель
+        for (int i = 0; i < num_fields; i++) {
+            if (i > 0) result_str += "-+-";
+            result_str += "---";
+        }
+        result_str += "\n";
+
+        // Данные
+        int row_count = 0;
+        while ((row = mysql_fetch_row(result))) {
+            for (int i = 0; i < num_fields; i++) {
+                if (i > 0) result_str += " | ";
+                result_str += row[i] ? row[i] : "NULL";
+            }
+            result_str += "\n";
+            row_count++;
+        }
+
+        result_str += "\nTotal: " + std::to_string(row_count) + " rows";
+        strncpy_s(response, sizeof(response), result_str.c_str(), _TRUNCATE);
+
+        mysql_free_result(result);
+    }
+    else {
+        // Не-SELECT запрос (INSERT, UPDATE, DELETE)
+        if (mysql_field_count(mysql) == 0) {
+            my_ulonglong affected_rows = mysql_affected_rows(mysql);
+            sprintf_s(response, sizeof(response), "Query executed successfully. Affected rows: %lld", affected_rows);
+        }
+        else {
+            sprintf_s(response, sizeof(response), "Query executed successfully");
+        }
+    }
+
+    return response;
+}
+
+//--------------------------------------------------
+DWORD WINAPI ClientHandler(LPVOID lpParam) {
+    WriteToLog("ClientHandler thread started");
+
+    SOCKET clientSocket = (SOCKET)lpParam;
+    char line[MAXLINE];
+    int n;
+
+    MYSQL* mysql = create_mysql_session();
+
+    if (mysql) {
+        WriteToLog("MySQL session created for client");
+    }
+    else {
+        WriteToLog("Failed to create MySQL session");
+        const char* error_msg = "Error: Cannot connect to database";
+        writen(clientSocket, error_msg, (int)strlen(error_msg) + 1);
+        closesocket(clientSocket);
+        return 1;
+    }
+
+    while (1) {
+        WriteToLog("Waiting for client data...");
+
+        n = readline(clientSocket, line, MAXLINE - 1);
+        if (n <= 0) {
+            WriteToLog(n == 0 ? "Client disconnected" : "Read error");
+            break;
+        }
+
+        WriteToLog("Received query");
+
+        // Log query
+        FILE* queryLog = fopen("queries.log", "a");
+        if (queryLog) {
+            fprintf(queryLog, "%s\n", line);
+            fclose(queryLog);
+        }
+
+        const char* result = execute_sql_query(mysql, line);
+
+        WriteToLog("Sending response...");
+        if (writen(clientSocket, result, (int)strlen(result) + 1) != strlen(result) + 1) {
+            WriteToLog("Write error to client");
+            break;
+        }
+
+        WriteToLog("Response sent successfully");
+    }
+
+    // Cleanup
+    if (mysql) {
+        mysql_close(mysql);
+        WriteToLog("MySQL connection closed");
+    }
+
+    WriteToLog("ClientHandler thread finished");
+    closesocket(clientSocket);
+    return 0;
+}
+
+//--------------------------------------------------
+int main(int argc, char* argv[]) {
+    WriteToLog("----- Server starting (MySQL C API) -----");
+
+    // Инициализируем MySQL client library
+    if (mysql_library_init(0, NULL, NULL)) {
+        WriteToLog("MySQL library initialization failed");
+        return 1;
+    }
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        WriteToLog("WSAStartup failed");
+        mysql_library_end();
+        return 1;
+    }
+    WriteToLog("WSAStartup OK");
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == INVALID_SOCKET) {
+        WriteToLog("Cannot create socket");
+        WSACleanup();
+        mysql_library_end();
+        return 1;
+    }
+    WriteToLog("Socket created");
+
+    // Setup server address
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(SERV_TCP_PORT);
+
+    // Bind
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == SOCKET_ERROR) {
+        WriteToLog("Cannot bind address");
+        closesocket(sockfd);
+        WSACleanup();
+        mysql_library_end();
+        return 1;
+    }
+    WriteToLog("Bind OK");
+
+    // Listen
+    if (listen(sockfd, 5) == SOCKET_ERROR) {
+        WriteToLog("Listen failed");
+        closesocket(sockfd);
+        WSACleanup();
+        mysql_library_end();
+        return 1;
+    }
+    WriteToLog("Listen OK");
+
+    printf("Server listening on port %d...\n", SERV_TCP_PORT);
+    WriteToLog("Server ready for connections");
+
+    // Main accept loop
+    while (1) {
+        WriteToLog("Waiting for connections...");
+
+        struct sockaddr_in cli_addr;
+        int clilen = sizeof(cli_addr);
+
+        SOCKET newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
+
+        if (newsockfd == INVALID_SOCKET) {
+            WriteToLog("Accept error");
+            Sleep(1000);
+            continue;
+        }
+
+        WriteToLog("New connection accepted");
+
+        // Create thread for client
+        HANDLE thread = CreateThread(NULL, 0, ClientHandler, (LPVOID)newsockfd, 0, NULL);
+        if (thread == NULL) {
+            WriteToLog("Failed to create client thread");
+            closesocket(newsockfd);
+        }
+        else {
+            CloseHandle(thread);
+            WriteToLog("Client thread created successfully");
+        }
+    }
+
+    closesocket(sockfd);
+    WSACleanup();
+    mysql_library_end();
+    WriteToLog("Server shutdown complete");
+    return 0;
+}
